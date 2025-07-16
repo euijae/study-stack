87776939d2c52484db96142b0c598a7b9aabedef
Euijae Kim <8512075+euijae@users.noreply.github.com>
2025-07-16 12:32:00 -0400

    prep

---
+++ coding/making-a-large-island-variant.py
@@ -0,0 +1,38 @@
+from typing import List
+
+class Solution:
+    def __init__(self):
+        self.directions = [(-1,0),(0,1),(1,0),(0,-1)]
+    
+    def borders_land(self, grid, r, c):
+        for dr, dc in self.directions:
+            nr, nc = r+dr, c+dc
+            if nr < 0 or nr >= len(grid):
+                continue
+            if nc < 0 or nc >= len(grid[0]):
+                continue
+            if grid[nr][nc] == 0:
+                continue
+            return True
+        return False
+
+    def create_island(self, grid, visited, r, c):
+        visited[r][c] = True
+        size = 1
+        for dr, dc in self.directions:
+            nr, nc = r+dr, c+dc
+            if nr < 0 or nr >= len(grid) or nc < 0 or nc >= len(grid[0]) or visited[nr][nc]:
+                continue
+            size += self.create_island(grid, visited, nr, nc)
+        return size
+    
+    def largestIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        visited = [[False] * cols for _ in range(rows)]
+        largest = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 or visited[r][c] or self.boarders_land(grid, r, c):
+                    continue
+                largest = max(largest, self.create_island(grid, visited, r, c))
+        return largest
