74094e5c9ab1599b6c3f57513a12ea02d301a380
Euijae Kim <8512075+euijae@users.noreply.github.com>
2025-07-18 21:42:31 -0400

    meta prep

---
+++ coding/kth-smallest-digit-sum.py
@@ -0,0 +1,24 @@
+import heapq
+
+class Solution:
+    def compute(self, num):
+        res = 0
+        while num:
+            res += num % 10
+            num //= 10
+            
+        return res
+
+    def kth_smallest_digit_sum(self, nums, k):
+        heap = []
+        for i, num in enumerate(nums):
+            dsum = self.compute(num)
+            print(dsum)
+            heapq.heappush(heap, (-dsum, -i, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return [num for _, _, num in heap]
+
+solution = Solution()
+print(solution.kth_smallest_digit_sum([111, 84, 21, 12, 3, 56, 2001, 10000], 2))
