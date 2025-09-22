f61176adc77665ff314a42f62b9f5a71164783d5
Euijae Kim <8512075+euijae@users.noreply.github.com>
2025-09-22 11:05:35 -0400

    update txt

--- coding/python/parse-txt.py
+++ coding/python/parse-txt.py
@@ -1,3 +1,6 @@
 with open("../file/example.txt", newline="") as f:
+    data1 = f.read()
+    data2 = f.readlines() # load the file all at once - good if you need all of them and a random access
+    # iterate line by line
     for line in f:
         print(line, end="")
